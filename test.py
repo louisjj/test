79bfcbcdf2de5f0423b74c7d8f985494ce37dff7
lj <l.jeannelle@gmail.com>
2016-03-20 18:08:40 +0100

    Testing simple flask app

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import unittest
+from app import app
 
 class Test(unittest.TestCase):
+	def setUp(self):
+		self.app = app.test_client()
+		self.app.testing = True
+
   def test_one(self):
     self.assertEquals(2, 2)
+    result = self.app.get('/')
+    self.assertEquals(result.status_code, 200)
+    self.assertEquals(result.data, "hello")
+
 
 if __name__ == '__main__':
   unittest.main()
