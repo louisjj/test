0f49e38b87ebc636dff91e61a4ce5004c348886a
lj <l.jeannelle@gmail.com>
2016-03-20 18:13:29 +0100

    Fixing bad indentation

--- test.py
+++ test.py
@@ -6,12 +6,11 @@ class Test(unittest.TestCase):
 		self.app = app.test_client()
 		self.app.testing = True
 
-  def test_one(self):
-    self.assertEquals(2, 2)
-    result = self.app.get('/')
-    self.assertEquals(result.status_code, 200)
-    self.assertEquals(result.data, "hello")
-
+  	def test_one(self):
+	    self.assertEquals(2, 2)
+	    result = self.app.get('/')
+	    self.assertEquals(result.status_code, 200)
+	    self.assertEquals(result.data, "hello")
 
 if __name__ == '__main__':
   unittest.main()
